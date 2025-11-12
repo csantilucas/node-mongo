56ec6dd7886dc1681ceb80cb437ace87b4f92a3d
csantilucas <lucascsanti2006@gmail.com>
2025-11-12 18:22:00 -0400

    add  post de link

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -3,19 +3,34 @@ const router = express.Router()
 import link from '../models/link.js';
 
 router.get("/title/:title", async (req, res) => {
-            try {
-                let title = req.params.title;
-                let doc = await link.findOne({ title })
+    try {
+        let title = req.params.title;
+        let doc = await link.findOne({ title })
 
-                res.redirect(doc.url)
-            } catch (erro) {
-                res.send(erro)
-            }
-        });
+        res.redirect(doc.url)
+    } catch (erro) {
+        res.send(erro)
+    }
+});
 
-        
+
 router.get('/', (req, res) => {
     res.send('hello server')
+})
+
+
+
+router.post( '/', express.urlencoded(), async (req, res) => {
+
+    let newlink = new link(req.body)
+
+    try {
+        let doc = await newlink.save()
+        res.send(doc)
+    } catch (erro) {
+        res.send(erro)
+    }
+
 })
 
 
